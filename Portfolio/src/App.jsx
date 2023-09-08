46541bbf9e53061ad39a33f528ce3902a113c60b
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-09-08 21:44:29 +0600

    file ready

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter} from 'react-router-dom';
+
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components';
+
+
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-      https://www.youtube.com/watch?v=0fYi8SGA20k&list=RDCMUCmXmlB4-HJytD7wek0Uo97A&start_radio=1&t=369s&ab_channel=JavaScriptMastery
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar/>
+            <Hero/>
+          </div>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className='relative z-0'>
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
